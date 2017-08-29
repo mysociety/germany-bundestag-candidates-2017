f848c6d2d8cf3da601a40cb5200be15dc804bbf3
Nick Jackson <nick@nickjackson.me>
2017-08-29 13:42:33 +0100

    Scraping, v1

--- scraper.py
+++ scraper.py
@@ -1,24 +1,36 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import json
+import sqlite3
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+# Get the JSON
+json = json.loads(scraperwiki.scrape('https://wahl.tagesspiegel.de/2017/api/candidates/dump'))
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# pprint.pprint(json)
+
+candidates = []
+
+for candidate in json['result']:
+
+    facebook_urls = []
+
+    for link in candidate['links']:
+        if link['type'] == 'facebook':
+            facebook_urls.append(link['url'])
+
+    candidate = {
+        'id': candidate['id'],
+        'name': candidate['name']['forename'] + candidate['name']['surname'],
+        'area_id': candidate['election']['district'],
+        'area_type_description': 'District',
+        'facebook_urls': ', '.join(facebook_urls)
+    }
+
+    candidates.append(candidate)
+
+try:
+    scraperwiki.sqlite.execute('DELETE FROM data')
+except sqlite3.OperationalError:
+    pass
+scraperwiki.sqlite.save(
+    unique_keys=['id', 'name', 'area_id', 'area_type_description', 'facebook_urls'],
+    data=candidates)
